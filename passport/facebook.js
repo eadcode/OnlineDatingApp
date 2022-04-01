aa635c28b407ec5da956ca580aba2ddb92506db7
eadcode <eadcodes@gmail.com>
2022-04-01 09:09:52 -0400

    Add passport, mongoDB

---
+++ passport/facebook.js
@@ -0,0 +1,43 @@
+const passport = require('passport');
+const Strategy = require('passport-facebook');
+const User = require('../models/user');
+const Keys = require('../config/keys');
+
+module.exports = function () {
+
+    // Configure the Facebook strategy for use by Passport.
+    //
+    // OAuth 2.0-based strategies require a `verify` function which receives the
+    // credential (`accessToken`) for accessing the Facebook API on the user's
+    // behalf, along with the user's profile.  The function must invoke `cb`
+    // with a user object, which will be set at `req.user` in route handlers after
+    // authentication.
+    passport.use(new Strategy({
+        clientID: Keys.FacebookAppID,
+        clientSecret: Keys.FacebookAppSecret,
+        // callbackURL: '/oauth2/redirect/www.facebook.com',
+        callbackURL: 'http://localhost:3000/auth/facebook/callback',
+        state: true,
+    }, (accessToken, refreshToken, profile, done) => {
+        console.log(profile);
+    }));
+
+    // Configure Passport authenticated session persistence.
+    //
+    // In order to restore authentication state across HTTP requests, Passport needs
+    // to serialize users into and deserialize users out of the session.  In a
+    // production-quality application, this would typically be as simple as
+    // supplying the user ID when serializing, and querying the user record by ID
+    // from the database when deserializing.  However, due to the fact that this
+    // example does not have a database, the complete Facebook profile is serialized
+    // and deserialized.
+    passport.serializeUser(function (user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function (id, done) {
+        User.findById(id, (err, user) => {
+            done(err, user);
+        });
+    });
+};
--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
 const { engine } = require('express-handlebars');
+
+const Message = require('./models/message');
+const User = require('./models/user');
+const Keys = require('./config/keys');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Body parser, reading data from body into req.body
+app.use(express.urlencoded({ extended: false, limit: '10kb' }));
+app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
+app.use(session({
+    secret: 'mysecret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.engine('handlebars', engine({ defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -25,6 +46,43 @@ app.get('/contact', (req, res) => {
         title: 'Contact'
     });
 });
+
+app.post('/contactUs', (req, res) => {
+    console.log(req.body)
+    const newMessage = {
+        fullname: req.body.fullname,
+        email: req.body.email,
+        message: req.body.message,
+        date: new Date()
+    }
+
+    new Message(newMessage).save((err, message) => {
+        if (err) {
+            throw err;
+        } else {
+            Message.find({}).then((messages) => {
+                if (messages) {
+                    res.render('newmessage', {
+                        title: 'Sent',
+                        messages: messages
+                    });
+                } else {
+                    res.render('nomessage', {
+                        title: 'Not Found'
+                    });
+                }
+            });
+        }
+    });
+});
+
+mongoose
+    .connect(Keys.MongoDB, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    })
+    .then(() => console.log('DB connection successful!'))
+    .catch(err => console.log(err));
 
 app.listen(port, () => {
     console.log(`Server is running on  port : ${port}`)
