86b99709524916a2c9f52c20b1b46e1d7de63417
eadcode <eadcodes@gmail.com>
2022-04-02 09:43:29 -0400

    add prod var

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+    MongoDB: process.env.MONGO_DB,
+    FacebookAppID: process.env.FACEBOOK_ID,
+    FacebookAppSecret: process.env.FACEBOOK_SECRET,
+    GoogleClientID: process.env.GOOGLE_ID,
+    GoogleClientSecret: process.env.GOOGLE_SECRET
+}
